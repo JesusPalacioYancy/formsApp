790cb7775511f6ce75a63c6af8432f855f86166f
Jesus Palacio <palacioj104@gmail.com>
2024-11-03 16:59:37 -0500

    Inicio seccion 18 | cap 257

--- src/app/auth/pages/resgisterPage/resgister-page.component.ts
+++ src/app/auth/pages/resgisterPage/resgister-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import * as customValidators from '../../../shared/validators/validators';
 import { ValidatoresService } from '../../../shared/services/validators.service';
+import { EmailValidators } from '../../../shared/validators/email-validators.service';
 
 @Component({
   templateUrl: './resgister-page.component.html',
@@ -10,12 +11,13 @@ export class ResgisterPageComponent {
 
   constructor(
     private fb: FormBuilder,
-    private validatoresService: ValidatoresService
+    private validatoresService: ValidatoresService,
+    private emailValidators: EmailValidators
   ){}
 
   public myFormReguister: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.validatoresService.firstNameAndLastnamePattern)] ],
-    email: ['', [Validators.required, Validators.pattern(this.validatoresService.emailPattern)] ],
+    email: ['', [Validators.required, Validators.pattern(this.validatoresService.emailPattern)], [this.emailValidators]  ],
     username: ['', [Validators.required, this.validatoresService.cantBeStraider ]],
     password: ['', [Validators.required, Validators.minLength(6)] ],
     password2: ['', [Validators.required ] ],
